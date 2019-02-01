fdb6f1b90a45679d5e6438819dc978593e8e400e
Spencer Brooks <spencer@databit.io>
2019-01-31 20:28:00 -0700

    forEach exercises

--- main.js
+++ main.js
@@ -1,24 +1,53 @@
 
 
-var colors = ['red', 'blue', 'green']
+// var colors = ['red', 'blue', 'green']
 
-for( var i = 0; i < colors.length; i++) {
-  console.log(colors[i])
+// for( var i = 0; i < colors.length; i++) {
+//   console.log(colors[i])
+// }
+
+// colors.forEach(function(color){
+//   console.log(color)
+// })
+
+// // create array of numbers
+// var numbers = [1,2,3,4,5]
+// // variable to hold  sum
+// var sum = 0
+
+// function adder(number){
+//   sum += number
+// }
+// // loop over array, incrementing the sum variable
+// numbers.forEach(adder)
+// // print the sum variable
+// console.log(sum)
+
+// **************************
+function handlePosts() {
+  var posts = [
+    { id: 23, title: 'Daily JS News' },
+    { id: 52, title: 'Code Refactor City' },
+    { id: 105, title: 'The Brightest Ruby' }
+  ];
+  
+  // for (var i = 0; i < posts.length; i++) {
+  //   savePost(posts[i]);
+  // }
+  posts.forEach(function(){
+      savePost(posts[i]);
+  });
 }
+// **********************************
 
-colors.forEach(function(color){
-  console.log(color)
-})
+var images = [
+  { height: 10, width: 30 },
+  { height: 20, width: 90 },
+  { height: 54, width: 32 }
+];
+var areas = [];
 
-// create array of numbers
-var numbers = [1,2,3,4,5]
-// variable to hold  sum
-var sum = 0
-
-function adder(number){
-  sum += number
-}
-// loop over array, incrementing the sum variable
-numbers.forEach(adder)
-// print the sum variable
-console.log(sum)
+images.forEach(function(image){
+    areas.push(image['height'] * image['width']);
+});
+// *********************************
