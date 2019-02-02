636398acdc7b76cc348c1e25fd85801c6bf8c01e
Spencer Brooks <spencer@databit.io>
2019-02-01 21:57:33 -0700

    map practice 3

--- main.js
+++ main.js
@@ -66,25 +66,50 @@
 // console.log(doubled)
 // *******************************
 
-var cars = [
-  { model: 'Model T', price: 'CHEAP' },
-  { model: 'camaro', price: 'expensive' },
-]
+// var cars = [
+//   { model: 'Model T', price: 'CHEAP' },
+//   { model: 'camaro', price: 'expensive' },
+// ]
 
-var prices = cars.map(function(car) {
-  return car.price
-})
+// var prices = cars.map(function(car) {
+//   return car.price
+// })
 
-console.log(prices)
+// console.log(prices)
 
-var images = [
-  { height: '34px', width: '39px' },
-  { height: '54px', width: '19px' },
-  { height: '83px', width: '75px' },
+// var images = [
+//   { height: '34px', width: '39px' },
+//   { height: '54px', width: '19px' },
+//   { height: '83px', width: '75px' },
+// ];
+
+// var heights = images.map(function(image) {
+//   return image.height
+// });
+
+// console.log(heights)
+
+var trips = [
+  { distance: 34, time: 10 },
+  { distance: 90, time: 50 },
+  { distance: 59, time: 25 }
 ];
 
-var heights = images.map(function(image) {
-  return image.height
+var speeds = trips.map(function(trip) {
+  return trip.distance / trip.time
 });
 
-console.log(heights)
+console.log(speeds)
+
+var paints = [ { color: 'red' }, { color: 'blue'}, { color: 'yellow'}];
+
+function pluck(array, property) {
+  var plucked = array.map(function(item) {
+    return item[property]
+  })
+  return plucked
+}
+
+
+
+
