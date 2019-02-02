a063525370ba2b39030a9ca9c578def504b4e223
Spencer Brooks <spencer@databit.io>
2019-02-01 20:56:27 -0700

    map exercise 1

--- main.js
+++ main.js
@@ -52,15 +52,39 @@
 // });
 // *********************************
 
-var numbers = [1,2,3]
-var doubledNumbers = []
-for (var i = 0; i < numbers.length; i++) {
-  doubledNumbers.push(numbers[i] * 2)
-}
+// var numbers = [1,2,3]
+// var doubledNumbers = []
+// for (var i = 0; i < numbers.length; i++) {
+//   doubledNumbers.push(numbers[i] * 2)
+// }
 
-var doubled = numbers.map(function(number) {
-  return number * 2
+// var doubled = numbers.map(function(number) {
+//   return number * 2
+// })
+
+// console.log(doubledNumbers)
+// console.log(doubled)
+// *******************************
+
+var cars = [
+  { model: 'Model T', price: 'CHEAP' },
+  { model: 'camaro', price: 'expensive' },
+]
+
+var prices = cars.map(function(car) {
+  return car.price
 })
 
-console.log(doubledNumbers)
-console.log(doubled)
+console.log(prices)
+
+var images = [
+  { height: '34px', width: '39px' },
+  { height: '54px', width: '19px' },
+  { height: '83px', width: '75px' },
+];
+
+var heights = images.map(function(image) {
+  return image.height
+});
+
+console.log(heights)
