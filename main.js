0ef73385652f0f9b35b7b8d858c4180dc9b3059e
Spencer Brooks <spencer@databit.io>
2019-02-03 21:20:52 -0700

    find exercise one

--- main.js
+++ main.js
@@ -219,3 +219,28 @@ var car = cars.find(function(car) {
 })
 
 console.log(car)
+
+var posts = [
+  { id: 1, title: 'New Post' },
+  { id: 2, title: 'Old Post' },
+]
+
+var comment = { postId: 1, content: 'Great Post' }
+
+function postForComment(posts, comment) {
+  return posts.find(function(post) {
+    return post.id === comment.postId
+  })
+}
+
+console.log(postForComment(posts, comment))
+
+var users = [
+  { id: 1, admin: false },
+  { id: 2, admin: false },
+  { id: 3, admin: true }
+];
+
+var admin = users.find(function(user) {
+    return user.admin === true
+});
